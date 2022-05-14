bd7ef1eeed335b9e73995018379ecbf7e2885e6d
JoshnaksPNG <80712017+JoshnaksPNG@users.noreply.github.com>
2022-05-14 13:24:04 -0700

    Misc Front End Work

--- express/tabemono.js
+++ express/tabemono.js
@@ -8,3 +8,27 @@ canvas.height = 650;
 //Define the canvas as a 2d canvas
 const ctx = canvas.getContext('2d');
 
+//Loop Function
+window.main = () =>
+{
+    window.requestAnimationFrame(main);
+}
+
+//Call Loop
+main();
+
+window.addEventListener('mousedown', (e) => 
+{
+
+});
+
+window.addEventListener('mousemove', (e) =>
+{
+
+});
+
+window.addEventListener('mouseup', (e) =>
+{
+
+});
+
